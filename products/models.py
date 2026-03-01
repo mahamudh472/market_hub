a656b2b3d976497fd7bb8c5877afb0dcb3f741fd
mahamudh472 <mahadymahamudh472@gmail.com>
2026-03-02 00:55:58 +0600

    model design start

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from="name", unique=True, always_update=False)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField(upload_to='category_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    pass
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='product_images/')
+    thumbnail = models.BooleanField(default=False)
