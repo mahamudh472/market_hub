55a5bac491c191460d6b404ba9a15b9e06eb04fc
Md Mahmud Hasan <mahadymahamudh472@gmail.com>
2026-03-25 23:07:23 +0600

    admin dashboard start

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from accounts.models import User
+from accounts.serializers import UserSerializer
 
-# Create your views here.
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
+
